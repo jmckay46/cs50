bd5bdc00140fbc78a759b38681d2d9791f17b965
Jonathan Mckay <jmckay586@gmail.com>
2018-03-05 20:56:33 +0000

    init_1

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -2,8 +2,23 @@
 #include<stdio.h>
 #include<string.h>
 
-int main(int argc, string argv[])                   //argv is an array of strings
+/* to do:
+1) get the key
+2) get the plain text
+3) encipher
+4) print cipher text
+*/
+
+
+int main(void)                   //normally we'd use the argv & string, but for now we're going to prompt the user input for a "key"
 {
-    string plain_txt = get_string("plaintext: ");
+
+    string plain_text = get_string("plaintext: ");
+    int key = get_int("key: ");
+
+        for (int i = 0; i < strlen(plain_text); i++)
+     {
+        printf("%c %i\n", plain_text[i], (int)plain_text[i+1]);
+     }
 
 }
