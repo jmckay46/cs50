9d2db668eb909296e4d3347b57fd8497a2860661
Jonathan Mckay <jmckay586@gmail.com>
2018-03-05 19:41:16 +0000

    init_1

---
+++ caesar.c
@@ -0,0 +1,9 @@
+#include<cs50.h>
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, string argv[])                   //argv is an array of strings
+{
+    string plain_txt = get_string("plaintext: ");
+
+}
