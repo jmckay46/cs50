131893c0172030adf91ae9afd7ec50b4b19d05f6
Jonathan Mckay <jmckay586@gmail.com>
2018-03-08 02:00:46 +0000

    init_1

--- pset1/mario_more_comfortable.c
+++ pset1/mario_more_comfortable.c
@@ -1,34 +1,48 @@
 #include<stdio.h>
 #include<cs50.h>
 
+void mario(int yNum);
+
 int main(void)
 {
-    int uNum;                                   //initialize user input as an int
-    do                                          //repeatedly prompt the user input for a non-negative integer
+
+    int uNum;
+
+    do
     {
-         uNum = get_int("Height: ");            //prompt the user for the height of mario's double pyramid
+        uNum = get_int("Height: ");                          //repeatedly prompt the user for a positive integer
     }
 
     while(uNum < 0 || uNum > 23);
 
-    for(int i = 0; i < uNum; i++ )                  //this loop prints one row and loops again until i is less than the users input
+    mario(uNum);
+
+}//end main
+
+
+void mario(int yNum)
+{
+
+    for(int i = 0; i < yNum; i++ )                          //this loop prints one row and loops again until i is less than the users input
         {
-            for (int j = uNum; j > i+1; j--)          //this loop prints the number of spaces per row
+            for (int j = yNum; j > i+1; j--)                //this loop prints the number of spaces per row
                 {
                     printf(" ");
                 }
-            for (int l = 0; l <= i; l++)     //this loop prints the number of hashes per row
+            for (int l = 0; l <= i; l++)                    //this loop prints the number of hashes per row
                 {
                     printf("#");
                 }
 
-            printf("  ");                   //print 2 spaces after the printed hashes
+            printf("  ");                                   //print 2 spaces after the printed hashes
 
-            for (int k = 0; k <= i; k++)    //print the same number of hashes again for the full double pyramid
+            for (int k = 0; k <= i; k++)                    //print the same number of hashes again for the full double pyramid
                 {
                     printf("#");
                 }
 
-            printf("\n");               //print a new line
+            printf("\n");                                   //print a new line for each loop
+
         } //end of main for loop
-}
+
+}//end mario
