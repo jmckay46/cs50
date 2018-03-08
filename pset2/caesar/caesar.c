7baaf4d15b9b8d228aef5c044490032d46e0fa33
Jonathan Mckay <jmckay586@gmail.com>
2018-03-08 04:08:11 +0000

    init_1

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -2,6 +2,7 @@
 #include<cs50.h>
 #include<string.h>
 #include<stdlib.h>
+#include<ctype.h>
 
 /*          argc is the argument counter             */
 /*          argv[] is the arguments passed to the program */
@@ -13,14 +14,27 @@ int main (int argc, string argv[])
         string p_text = get_string("plaintext: ");               //prompt the user for a string
         int key = atoi(argv[1]);                                //store the first argument into integer key
         int j = strlen(p_text);                                 //store the total length of users input into integer j
-
+        char c_text;
         printf("ciphertext: ");
-        for (int n = 0; n < j; n ++)                            //loop j amount of times, which is the users input
+        for (int n = 0; n < j; n ++)                            //loop until the end of the string
             {
-                char c_text = p_text[n] + key;
-                printf("%c", c_text);
+
+                     c_text = p_text[n] + key;                  //store the nth character + key in c_text
+
+                     if(isspace(p_text[n]))                     //print a space when a space is given, we don't want to make any changes to spaces.
+                        {
+                            printf(" ");
+                        }
+                    else
+                        {
+                            printf("%c", c_text);               //print each ciphered character, sequentially
+                        }
             }
+
     printf("\n");
     printf("Program Counter is %i\n", argc);
 
 }//end main
+
+
+
