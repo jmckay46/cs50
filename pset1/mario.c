0ecc35bd56bf4b46316730f1c7528151d3afd197
Jonathan Mckay <jmckay586@gmail.com>
2018-03-08 01:44:59 +0000

    init_1

--- pset1/mario.c
+++ pset1/mario.c
@@ -1,36 +1,43 @@
 #include<stdio.h>
 #include<cs50.h>
 
-int main(void)
-{
+
+void mario(int yNum);
+
+int main(void){
+
     int uNum;
-    do
-    {
-         uNum = get_int("Height: ");
-    }
 
-    while(uNum <= 0);
-
-    printf("\n");
-
-    for(int i = 0; i < uNum; i++ )
-        {
-            for (int j = uNum; j > i; j--)     //prints no of spaces
-                {
-                    printf(" ");
-                }
-            for (int l = 0; l <= i; l++)     //prints no of hashes
-                {d
-                    printf("#");
-                }
-
-            printf("  ");
-
-            for (int k = 0; k <= i; k++)
-                {
-                    printf("#");
-                }
-
-            printf("\n");
+    do  {                                               //repeatedly prompt the user for a positive integer
+        uNum = get_int("Height: ");
         }
-}
+
+    while(uNum < 0 || uNum > 23);
+
+    mario(uNum);
+
+
+}//end main
+
+void mario(int yNum)
+{
+        yNum = yNum +1;
+
+        for(int i = 1; i < yNum; i++)
+                {                                   //print this many columns, changed i to 1 since it's not a full pyramid
+
+                    for(int l =yNum; l > i+1; l--)
+                        {                           //print this many spaces, decrement the amount of printed spaces
+                            printf(" ");
+                        }
+
+                    for(int j =0; j <= i; j++)
+                        {                           //print this many blocks, increment the amount of printed blocks
+                            printf("#");
+                        }
+
+                    printf("\n");                   //print a new line per loop
+
+                 } //end for loop
+
+}//end mario
