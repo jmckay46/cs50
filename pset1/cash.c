3270371b03ad3957f2a4407a1d3a6d7dd7b06727
Jonathan Mckay <jmckay586@gmail.com>
2018-03-08 03:11:03 +0000

    init_1

--- pset1/cash.c
+++ pset1/cash.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 #define QUARTER 25
 #define DIME 10
 #define NICKEL 5
 #define PENNY 1
 
+void counter(int amount);
+
 int main(void)
 {
     float input;
-    int amount;
-    int count = 0;
 
     do
         {
-            input = get_float("Change Owed: ");
+            input = get_float("Change Owed: ");                     //prompt the user for a float type
         }
 
     while(input <= 0);
 
     input = input*100;
+    input = round(input);
+    counter(input);
 
-    amount = (int) input;
+}//end main
+
+
+void counter(int amount)
+{
+    int count = 0;
 
       while(amount >= QUARTER)
     {
-        count ++;                                                //increment the coin count per quarter
+        count ++;                                                    //increment the coin count per quarter
         amount = amount - QUARTER;
     }
 
-    while(amount >= DIME)                                       //if the amount is greater than or equal to a 10 cents, then loop
+    while(amount >= DIME)                                            //if the amount is greater than or equal to a 10 cents, then loop
     {
         count ++;
         amount = amount - DIME;
@@ -37,18 +45,17 @@ int main(void)
 
     while(amount >= NICKEL)
     {
-        count ++;                                               //increment the coin count per nickel
+        count ++;                                                    //increment the coin count per nickel
         amount = amount - NICKEL;
     }
 
-    while(amount >= PENNY)           //if the current amount is greater than or equal to one cent, then loop
+    while(amount >= PENNY)                                           //if the current amount is greater than or equal to one cent, then loop
     {
-        count ++;                    //increment the coin count per penny
+        count ++;                                                    //increment the coin count per penny
         amount = amount - PENNY;
     }
 
 
-     printf("%i\n", count);                 //output the number of coins dispensed
+     printf("%i\n", count);                                         //output the number of coins dispensed
 
-    //printf("%f\n", amount);         //used to test output the amount
-
+}//end of counter function
