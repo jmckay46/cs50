5cfafaaf80dbb26e4fad16761fa15fd1bd68eef9
Jonathan Mckay <jmckay586@gmail.com>
2018-03-09 01:24:59 +0000

    init_1

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -4,37 +4,42 @@
 #include<stdlib.h>
 #include<ctype.h>
 
-/*          argc is the argument counter             */
-/*          argv[] is the arguments passed to the program */
-/*          therefore argv[1] must be the key       */
+/*          argc is the argument counter                                                               */
+/*          argv[] is the arguments passed to the program                                              */
+/*          therefore argv[1] must be the key                                                          */
+
+void cipher(string text, int k);
 
 int main (int argc, string argv[])
 {
 
         string p_text = get_string("plaintext: ");               //prompt the user for a string
         int key = atoi(argv[1]);                                //store the first argument into integer key
-        int j = strlen(p_text);                                 //store the total length of users input into integer j
-        char c_text;
         printf("ciphertext: ");
-        for (int n = 0; n < j; n ++)                            //loop until the end of the string
-            {
+        cipher(p_text, key);
 
-                     c_text = p_text[n] + key;                  //store the nth character + key in c_text
-
-                     if(isspace(p_text[n]))                     //print a space when a space is given, we don't want to make any changes to spaces.
-                        {
-                            printf(" ");
-                        }
-                    else
-                        {
-                            printf("%c", c_text);               //print each ciphered character, sequentially
-                        }
-            }
-
-    printf("\n");
-    printf("Program Counter is %i\n", argc);
+        printf("\n");
+        printf("Program Counter is %i\n", argc);
 
 }//end main
 
 
+void cipher(string text, int k)
+{
+    char ci_text;
+    int j = strlen(text);                                       //store the strings length into j
+    for (int n = 0; n < j; n++)                                 //loop until the end of the string
+    {
+        ci_text = text[n] + k;                                  //ci_text is the character at the nth plus the key
+
+        if(isspace(text[n]))                                    //if we run into a space in our string, then print the space
+        {
+            printf(" ");
+        }
+        else                                                    //else, print the ciphered character
+        {
+            printf("%c", ci_text);
+        }
+    }//end for
+}//end cipher
 
