c1c0ca0bb355f13355bab4857c4a4149a2fae912
Jonathan Mckay <jmckay586@gmail.com>
2018-03-15 11:29:46 +0000

    init_1

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -4,17 +4,16 @@
 #include<stdlib.h>
 #include<ctype.h>
 
-/*          argc is the argument counter                        */
-/*          argv[] is the arguments passed to the program       */
-/*          therefore argv[1] must be the key                   */
+/*          argc is the argument counter                                            */
+/*          argv[] is the arguments passed to the program                           */
+/*          therefore argv[1] must be the key                                       */
 
 void cipher(string text, int k);
 
 int main (int argc, string argv[])
 {
-
-        string p_text = get_string("plaintext: ");               //prompt the user for a string
-        int key = atoi(argv[1]);                                 //store the first argument into integer key
+        string p_text = get_string("plaintext: ");                              //prompt the user for a string
+        int key = atoi(argv[1]);                                                //store the first argument into integer key
         cipher(p_text, key);
         printf("\n");
         printf("Program Counter is %i\n", argc);
@@ -22,37 +21,58 @@ int main (int argc, string argv[])
 }//end main
 
 
-void cipher(string text, int k)                                 //this function prompts the user for a caesar's ciphered text & outputs the ciphered text
+void cipher(string text, int k)                                                 //this function prompts the user for a caesar's ciphered text & outputs the ciphered text
 {
-    printf("ciphertext0: ");
-    char ci_text;
-    int j = strlen(text);                                       //store the strings length into j
-    for (int n = 0; n < j; n++)                                 //loop until the end of the string
-    {
-        ci_text = text[n] + k;                                  //ci_text is the character at the nth plus the key
+    printf("ciphertext: ");
 
-        if(isspace(text[n]))                                    //if we run into a space in our string, then print the space
+    for(int n = 0; n < strlen(text); n++)                                      //one character at a time, at nth index of our string in plain text
         {
-            printf(" ");
-        }
-        else                                                    //else, print the ciphered character
-        {
-            printf("%c", ci_text);
-        }
-    }//end for
 
-    printf("\nciphertext1: ");
-    for (int l = 0; l < j; l++)
-    {
-        ci_text = text[l] + k;
-        if(isalpha(ci_text))                                    //if we run into a single ciphered alphabetical character
-        {
-            printf("%c", ci_text);                              //then print the ciphered alphabetical letter
-        }
-        else
-        {
-            ci_text = 'A' + l;                                  //if it's not an alphabet, add A + count
-            printf("%c", ci_text);
-        }
-    }
+            if(isupper(text[n]))
+            {
+                int ci_text = (int) text[n] - 65;                               //take the integer value of the ascii character and subtract by 65
+                ci_text = ci_text + k;
+                if (ci_text <= 25)
+                 {
+                    char c_text = (char) ci_text +65;
+                    printf("%c", c_text);
+                 }
+               else
+                 {
+                     char c_text = (char) ci_text - 26 + 65;
+                     printf("%c", c_text);
+                 }
+            ci_text = 0;
+            }
+
+            else if(islower(text[n]))
+             {
+                 int ci_text = (int) text[n] - 97;
+                 ci_text = ci_text + k;
+                 if (ci_text <= 25)
+                   {
+                     char c_text = (char) ci_text +97;
+                     printf("%c", c_text);
+                   }
+                 else
+                   {
+                     char c_text = (char) ci_text - 26 + 97;
+                     printf("%c", c_text);
+                   }
+                 ci_text =0;
+             }
+
+            else if(isspace(text[n]))                           //if we run into a space at nth character, print a space
+            {
+                printf(" ");
+            }
+
+            else if(ispunct(text[n]))                           //if we run into a punctuation at nth character, then print the punctuated character
+            {
+                char punct = text[n];
+                printf("%c", punct);
+            }
+
+        }//end of for
+
 }//end cipher
