8c9e2aebadabb68232d81abfa115f369e3acd33f
Jonathan Mckay <jmckay586@gmail.com>
2018-03-15 12:01:58 +0000

    init_1

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -1,37 +1,46 @@
+
 #include<stdio.h>
 #include<cs50.h>
 #include<string.h>
 #include<stdlib.h>
 #include<ctype.h>
 
-/*          argc is the argument counter                                            */
-/*          argv[] is the arguments passed to the program                           */
-/*          therefore argv[1] must be the key                                       */
+/*          argc is the argument counter                        */
+/*          argv[] is the arguments passed to the program       */
+/*          therefore argv[1] must be the key                   */
 
 void cipher(string text, int k);
 
 int main (int argc, string argv[])
 {
-        string p_text = get_string("plaintext: ");                              //prompt the user for a string
-        int key = atoi(argv[1]);                                                //store the first argument into integer key
+    if (argc <= 2)
+    {
+        string p_text = get_string("plaintext: ");               //prompt the user for a string
+        int key = atoi(argv[1]);                                 //store the first argument into integer key
         cipher(p_text, key);
         printf("\n");
         printf("Program Counter is %i\n", argc);
-
+    }
+    else
+    {
+        printf("Error! argc must be less than %i\n", argc);
+        return 1;
+    }
 }//end main
 
 
-void cipher(string text, int k)                                                 //this function prompts the user for a caesar's ciphered text & outputs the ciphered text
+void cipher(string text, int k)                                 //this function prompts the user for a caesar's ciphered text & outputs the ciphered text
 {
     printf("ciphertext: ");
 
-    for(int n = 0; n < strlen(text); n++)                                      //one character at a time, at nth index of our string in plain text
+    for(int n = 0; n < strlen(text); n++)                                  //one character at a time, at nth index of our string in plain text
         {
 
-            if(isupper(text[n]))
+              if(isupper(text[n]))
             {
-                int ci_text = (int) text[n] - 65;                               //take the integer value of the ascii character and subtract by 65
+                int ci_text = (int) text[n] - 65;                                 //take the integer value of the ascii character and subtract by 65
                 ci_text = ci_text + k;
+                ci_text = ci_text % 26;
                 if (ci_text <= 25)
                  {
                     char c_text = (char) ci_text +65;
@@ -39,7 +48,7 @@ void cipher(string text, int k)                                                 
                  }
                else
                  {
-                     char c_text = (char) ci_text - 26 + 65;
+                     char c_text = (char) ci_text + 65;
                      printf("%c", c_text);
                  }
             ci_text = 0;
@@ -49,6 +58,7 @@ void cipher(string text, int k)                                                 
              {
                  int ci_text = (int) text[n] - 97;
                  ci_text = ci_text + k;
+                 ci_text = ci_text % 26;
                  if (ci_text <= 25)
                    {
                      char c_text = (char) ci_text +97;
@@ -74,5 +84,4 @@ void cipher(string text, int k)                                                 
             }
 
         }//end of for
-
 }//end cipher
