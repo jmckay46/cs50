91f5ae759d14fc487493d8807ac6ebd0e8d7e39f
Jonathan Mckay <jmckay586@gmail.com>
2018-04-05 23:25:55 +0000

    init_1

--- pset1/credit.c
+++ pset1/credit.c
@@ -2,16 +2,24 @@
 #include<stdio.h>
 #include<math.h>
 
+
 int main(void)
 {
     printf("credit card number: 4519019868815974\n");
     long long cc_number = 4519019868815974;
-    int uNum;
-    int mod = 1;
+    long long uNum;
+    long long yNum;
+    long long mod = 100;
+    long long xNum;
 
+    for(int n = 1; n < 9; n++)                          //for generating the second to last digit in the credit card
+    {
+        printf("mod = %lld\n", mod);
         uNum = cc_number % mod;
-        mod = mod/10;
-        uNum = (cc_number - uNum % mod)/mod;
-        printf("%i\n", digit);                              //accessing the mod'th digit of cc_number
-
+        yNum = cc_number % (mod/10);
+        xNum = (uNum - yNum);
+        xNum = xNum/(mod/10);
+        printf("%lld\n", xNum);
+        mod *= 100;
+    }
 }
