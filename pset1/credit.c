182bc03ab2759f9628184f31875ac4f23536189c
Jonathan Mckay <jmckay586@gmail.com>
2018-04-06 01:48:34 +0000

    init_1

--- pset1/credit.c
+++ pset1/credit.c
@@ -11,26 +11,45 @@ int main(void)
     long long yNum;
     long long mod = 100;
     long long xNum;
+    long long sum;
+    long long rNum;
 
-    printf("first set: \n");
+    printf("first set: \n\n");
+    //here we need to multiply each digit, and then add each individual digit together
+
     for(int n = 1; n < 9; n++)                          //for generating the second to last digit on the credit card
     {
         uNum = cc_number % mod;
         yNum = cc_number % (mod/10);
-        xNum = (uNum - yNum);
-        xNum = xNum/(mod/10);
-        printf("%lld\n", xNum);
+        xNum = (uNum - yNum)/(mod/10);
+        printf("%lld", xNum);
+        sum = xNum*2;
+       // printf("*2 =  %lld", sum);
+
+        if(sum >= 10)
+        {
+            rNum = sum % 10;
+            sum = (sum - rNum)/10;
+            printf("  *2 and added: %lld\n", rNum + sum);
+        }
+        else
+        {
+            printf("  *2 and added: %lld\n", sum);
+        }
+
         mod *= 100;
     }
-    mod = 10;
-    printf("second set: \n");
-    for (int j = 0; j < 8; j++)                       //for generating the last to second last digit on the credit card
+    mod = 1000;
+    sum = cc_number % 10;
+    printf("\nsecond sets' sum: \n");
+    //here we need to add each digit and then take the sum.
+    for (int j = 0; j <= 6 ; j++)                           //for generating the last to second last digit on the credit card
     {
         uNum = cc_number % mod;
         yNum = cc_number % (mod/10);
-        xNum = (uNum - yNum);
-        xNum = xNum/(mod/10);
-        printf("%lld\n", xNum);
+        xNum = (uNum - yNum)/(mod/10);
         mod *= 100;
+        sum += xNum;
     }
+    printf("%lld\n", sum);
 }
