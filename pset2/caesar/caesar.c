290f8d03d4d86152f34c8160e9f8306c5e54d1c2
Jonathan Mckay <jmckay586@gmail.com>
2018-04-10 04:54:11 +0000

    init_1

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -4,82 +4,58 @@
 #include<stdlib.h>
 #include<ctype.h>
 
-/*                  This program prompts the user for a string
-                    and ciphers the string using caesar's
-                    algorithm                                       */
+#define ASCII_LWRCASE_INDEX 97
+#define ASCII_UPRCASE_INDEX 65
+#define A_TO_Z 26
 
-
-void cipher(string text, int k);
+char cipher(char cin, int key);
+int charIndex = 0;
 
 int main (int argc, string argv[])
 {
-    if (argc == 2)                                               //print a message error if the argc is not equal to 2
+    if (argc != 2)
     {
-        string p_text = get_string("plaintext: ");               //prompt the user for a string
-        int key = atoi(argv[1]);                                 //store the first argument into integer key
-        cipher(p_text, key);                                     //print the ciphered charater
+        printf("Usage: %s\n", argv[0]);
+        return 1;
     }
     else
     {
-        printf("Usage: ./caesar k");
-        return 1;
+        string text = get_string("plaintext: ");
+        int key = atoi(argv[1]);
+        for(int n = 0; n < strlen(text); n++)
+        {
+            text[n] = cipher(text[n], key);
+        }
+        printf("%s\n", text);
+        return 0;
     }
+
 }//end main
 
 
-void cipher(string text, int k)                                 //this function prints caesar's ciphered text
+char cipher(char cin, int k)
 {
-    printf("ciphertext: ");
-
-    for(int n = 0; n < strlen(text); n++)                       //loop until the end of the string
-        {
-
-              if(isupper(text[n]))                              //if we run into an uppercase..
-            {
-                int ci_text = (int) text[n] - 65;               //'zero' in the uppercase letters
-                ci_text = ci_text + k;                          //add the key value to the 'zeroed' integer value
-                ci_text = ci_text % 26;                         //mod that value by 26
-                if (ci_text <= 25)
-                 {
-                    char c_text = (char) ci_text +65;
-                    printf("%c", c_text);
-                 }
-               else
-                 {
-                     char c_text = (char) ci_text + 65;
-                     printf("%c", c_text);
-                 }
-            ci_text = 0;                                     //reset ci_text to 0 since we'll be looping again=
-            }
-
-            else if(islower(text[n]))                           //if we run into a lowercase..
-             {
-                 int ci_text = (int) text[n] - 97;
-                 ci_text = ci_text + k;
-                 ci_text = ci_text % 26;
-                 if (ci_text <= 25)
-                   {
-                     char c_text = (char) ci_text +97;
-                     printf("%c", c_text);
-                   }
-                 else
-                   {
-                     char c_text = (char) ci_text - 26 + 97;
-                     printf("%c", c_text);
-                   }
-                 ci_text = 0;
-             }
-
-            else if(isspace(text[n]))                           //if we run into a space at nth character, print a space
-            {
-                printf(" ");
-            }
-
-            else if(ispunct(text[n]))                           //if we run into a punctuation at nth character, then print the punctuated character
-            {
-                char punct = text[n];
-                printf("%c", punct);
-            }
-
-        }//end of for
-}//end cipher
+    if(isupper(cin))
+    {
+        cin -= ASCII_UPRCASE_INDEX;
+        cin = (cin + k) % A_TO_Z;
+        cin += ASCII_UPRCASE_INDEX;
+        charIndex++;
+    }
+    else if(islower(cin))
+    {
+        cin += ASCII_LWRCASE_INDEX;
+        cin = (cin + k) % A_TO_Z;
+        cin += ASCII_LWRCASE_INDEX;
+        charIndex++;
+    }
+    else if(isspace(cin))
+    {
+        printf("%c", cin);
+    }
+    else if(ispunct(cin))
+    {
+        printf("%c", cin);
+    }
+    return cin;
+}
