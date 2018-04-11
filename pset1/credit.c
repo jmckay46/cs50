a8248e92b21f826c474871ce7f9901d32317c152
Jonathan Mckay <jmckay586@gmail.com>
2018-04-11 09:22:06 +0000

    init_1

--- pset1/credit.c
+++ pset1/credit.c
@@ -2,38 +2,40 @@
 #include<stdio.h>
 #include<math.h>
 
-//int cc_length()                           find.length(cc_number), return int; length of credit card #
-int check_Luhn(long long, long long);         // returns check_sum
+#define NEXT_DIGIT 100
+
+int check_Luhn(long long, long long);         // checks to see if the given credit card number is valid
 
 int main(void)
 {
     long long cc_number = get_long_long("Number: ");
-    int check_sum = check_Luhn(cc_number, 10);
-    printf("checksum: %i\n", check_sum);
+    int valid = check_Luhn(cc_number, 10);
+    printf("sum should end in zero:  %i\n", valid);
     return 0;
 }//end main
 
-int check_Luhn(long long cc_in, long long mod)
+int check_Luhn(long long cc_in, long long modulo)
 {
     int sum = 0;
-    int e_digit = 0;
-    int o_digit = 0;
-    int e_sum = 0;
-    int o_sum = 0;
+    int even_digit = 0;
+    int odd_digit = 0;
+    int even_sum = 0;
+    int odd_sum = 0;
 
     for(int i = 0; i < 8; i++)
     {
-        e_digit = (cc_in % (mod * 10) - (cc_in % (mod)))/(mod);
-        o_digit = (cc_in % mod - (cc_in % (mod/10)))/(mod/10);
-        e_sum = e_digit * 2;
-        if (e_sum > 9)
+        even_digit = (cc_in % (modulo * 10) - (cc_in % (modulo)))/(modulo);
+        odd_digit = (cc_in % modulo - (cc_in % (modulo/10)))/(modulo/10);
+        even_sum = even_digit * 2;
+        if (even_sum > 9)
         {
-            e_sum = 1 + (e_sum % 10);
+            even_sum = 1 + (even_sum % 10);
         }
-        mod *= 100;
-        o_sum += o_digit;
-        sum += e_sum;
+        modulo *= NEXT_DIGIT;
+        odd_sum += odd_digit;                           //sum all odd digits, store it in odd_sum
+        sum += even_sum;                                //take the sum of all even digits that are multiplied by 2
     }
-
-    return sum + o_sum;
+    sum = sum + odd_sum;                                //take the sum of all odd digits and add it to the sum of all digits multiplied by 2
+    printf("checksum: %i\n", sum);
+    return sum % 10;
 }//end check_Luhn
