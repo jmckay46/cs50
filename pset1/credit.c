1e7b91e12eb80c8c725e1d1774b9b584390370e5
Jonathan Mckay <jmckay586@gmail.com>
2018-04-11 08:57:46 +0000

    init_1

--- pset1/credit.c
+++ pset1/credit.c
@@ -2,46 +2,40 @@
 #include<stdio.h>
 #include<math.h>
 
-int odd_digit(long long, long long);             //returns the sum of digits not multiplied by 2
-int even_digit(long long, long long);
-
-int digit = 0;
-int sum = 0;
+//int cc_length()                           find.length(cc_number), return int; length of credit card #
+int check_Luhn(long long, long long);         // returns check_sum
 
 int main(void)
 {
-    long long cc_number = get_long_long("Credit card number?: ");
-    int odd_sum = odd_digit(cc_number, 10);
-    int even_sum = even_digit(cc_number, 100);
-    int check_sum = odd_sum + even_sum;
+    long long cc_number = get_long_long("Number: ");
+    int check_sum = check_Luhn(cc_number, 10);
     printf("checksum: %i\n", check_sum);
     return 0;
 }//end main
 
-int odd_digit(long long y, long long o_mod)
+int check_Luhn(long long cc_in, long long mod)
 {
-    for (int i = 0; i < 8; i++)
-    {
-        digit = (y % o_mod - (y % (o_mod/10)))/(o_mod/10);
-        sum += digit;
-        o_mod *=100;
-    }
-    return sum;
-}
+    int sum = 0;
+    int e_digit = 0;
+    int o_digit = 0;
+    int e_sum = 0;
+    int o_sum = 0;
+    long long e_mod = 10 * mod;
 
-int even_digit(long long x, long long e_mod)                                    //e_mod is 100, returns the total sum of digits multiplied by 2
-{
-    int sum1 = 0;
-    for (int i = 0; i < 8; i++)                                                     //this loop
+    for(int i = 0; i < 8; i++)
     {
-        digit = (x % e_mod - (x % (e_mod/10)))/(e_mod/10);
-        sum = digit * 2;                                                                //multiply each digit by 2
-        if (sum > 9)                                                                //if the digit is greater than 9..
+        e_digit = (cc_in % e_mod - (cc_in % (e_mod/10)))/(e_mod/10);
+        o_digit = (cc_in % mod - (cc_in % (mod/10)))/(mod/10);
+        e_sum = e_digit * 2;
+        if (e_sum > 9)
         {
-            sum = 1 + (sum % 10);                                                   //add 1 + the ones' place
+            e_sum = 1 + (e_sum % 10);
         }
         e_mod *= 100;
-        sum1 += sum;
+        mod *= 100;
+        o_sum += o_digit;
+        sum += e_sum;
     }
-    return sum1;
-}
+
+    return sum + o_sum;
+}//end check_Luhn
