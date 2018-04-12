9d403c178fb3d5ce26790118da3fa33e28febb8c
Jonathan Mckay <jmckay586@gmail.com>
2018-04-12 06:51:18 +0000

    init_1

--- pset1/credit.c
+++ pset1/credit.c
@@ -15,33 +15,42 @@ int get_length(long long, long long);                       //returns the length
 int main(void)
 {
     long long cc_number = get_long_long("Number: ");
+    long long mod;
     int first_digit;
+    int second_digit;
     int valid = check_Luhn(cc_number, 10);
     int cc_length = get_length(cc_number, 10);
 
-    if (valid != 0 && cc_length < MIN_CC_LENGTH)
+    if (valid != 0 && cc_length > MIN_CC_LENGTH)
     {
         printf("INVALID\n");
     }
     else
     {
-        long long mod = pow(10, cc_length);
+        mod = pow(10, cc_length);
         first_digit = (cc_number - (cc_number % (mod/10)))/(mod/10);                //calculates & stores the first digit of the number
+        mod = pow(10, cc_length - 1);                                               //modulus of the second first digit
+        second_digit = (cc_number - (cc_number %(mod/10)))/(mod/10);                //store the first 2 digits of the number in second_digit
 
         if(first_digit == 4)
         {
             printf("VISA\n");
         }
-        else if(first_digit == 5)
+        else if(second_digit >= 51 && second_digit <= 55)
         {
-            printf("MasterCard\n");
+            printf("MASTERCARD\n");
         }
-        else if(first_digit == 3)
+        else if(second_digit >= 34 && second_digit <= 37)
         {
             printf("AMEX\n");
         }
+        else
+        {
+            printf("INVALID\n");
+        }
     }
     return 0;
+
 }//end main
 
 int check_Luhn(long long cc_in, long long modulo)
@@ -52,7 +61,7 @@ int check_Luhn(long long cc_in, long long modulo)
     int even_sum = 0;
     int odd_sum = 0;
 
-    for(int i = 0; i < 8; i++)
+    for(int i = 0; i < (MAX_CC_LENGTH)/2; i++)
     {
         even_digit = (cc_in % (modulo * 10) - (cc_in % (modulo)))/(modulo);
         odd_digit = (cc_in % modulo - (cc_in % (modulo/10)))/(modulo/10);
@@ -85,4 +94,3 @@ int get_length(long long cin, long long modulus)
     cc_length = log10(mod);
     return cc_length;
 }//end get_length
-
