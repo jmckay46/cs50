b1dfdba9b8176463a38ea9d7c9f21480c3e2959c
jmckay46 <jmckay586@gmail.com>
2018-11-02 20:41:45 +0000

    added a function that prints the binary value

--- ideas_dump/int_bin.c
+++ ideas_dump/int_bin.c
@@ -2,24 +2,32 @@
 #include<stdio.h>
 #include<math.h>
 
+void binary_value(int);
+
 int main()
 {
     int int_num = get_int("Integer value: ");
-    int n = log(int_num)/log(2);
-    char binary[65535];
+    binary_value(int_num);
+}
 
-    for(int i = n; i >= 0; i--)
+
+void binary_value(int int_val)
+{
+    char bin_val[65535];
+    int n = log(int_val)/log(2);
+
+    for(int i = n; i>=0; i--)
     {
-        if(int_num%2 == 1)
+        if(int_val%2 == 1)
         {
-            binary[i] = '1';
+            bin_val[i] = '1';
         }
         else
         {
-            binary[i] = '0';
+            bin_val[i] = '0';
         }
-        int_num = int_num/2;
-    }
 
-    printf("Binary Value: %s\n", binary);
+        int_val = int_val/2;
+    }
+    printf("Binary value: %s\n", bin_val);
 }
