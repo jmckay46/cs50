ddc179976a5454975406aad5733b0f47203977d5
jmckay46 <jmckay586@gmail.com>
2018-11-03 08:49:58 +0000

    finished integer to hexadecimal conversion

--- ideas_dump/hex.c
+++ ideas_dump/hex.c
@@ -6,16 +6,15 @@ int main()
 {
     int int_val = get_int("Integer value: ");
     int n = log(int_val)/log(16)+1;
-    int int_list[16] = {'0','1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-    char hex_val[n];
+    char hex_list[16] = {'0','1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    char hex_val[4096];
     int index;
 
-    for(int i = 0; i < n; i++)
+    for(int i = n; i >=0; i--)
     {
         index = int_val % 16;
-        hex_val[i] = int_list[index];
+        hex_val[i] = hex_list[index];
         int_val = int_val/16;
     }
-    printf("%s\n", hex_val);
-
+    printf("Hexadecimal value: 0x%s\n", hex_val);
 }
