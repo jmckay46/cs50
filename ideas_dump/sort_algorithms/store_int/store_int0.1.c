38487eeb4aa1c6dcb5dca0c15fbd0249c2090dc9
Jonathan McKay <jmckay586@gmail.com>
2019-12-27 19:13:41 +0000

    testing simple integer storage

---
+++ ideas_dump/sort_algorithms/store_int/store_int0.1.c
@@ -0,0 +1,15 @@
+#include<cs50.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(int argc, string argv[])
+{
+    int list[argc-1];
+
+    for(int i =0; i <= argc; i++)
+    {
+        list[i] = argv[i+1];
+    }
+
+}
