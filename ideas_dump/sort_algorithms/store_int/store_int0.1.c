2c9b78210e6a50ad783f7b1ad6c350b36005b65c
Jonathan McKay <jmckay586@gmail.com>
2019-12-27 19:37:40 +0000

    workin program that stores CLA into an array & prints

--- ideas_dump/sort_algorithms/store_int/store_int0.1.c
+++ ideas_dump/sort_algorithms/store_int/store_int0.1.c
@@ -2,16 +2,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//this program simply stores command line arguments into an array & prints the list.
 
 int main(int argc, string argv[])
 {
     string list[argc-1];
-
+    //take command line arguments and store it in list[]
     for(int i =0; i <= argc-1; i++)
     {
         list[i] = argv[i+1];
     }
-
+    //print the unsorted list
     for(int i = 0; i <= argc-1; i++)
     {
         printf("%s\n", list[i]);
