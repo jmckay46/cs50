6ddcf5b424dc6d1433f7af3f14d1df3caa824984
Jonathan McKay <jmckay586@gmail.com>
2020-01-02 02:56:34 +0000

    readability.c: added function that counts the number of words in a paragraph.

--- pset2_2019/readability.c
+++ pset2_2019/readability.c
@@ -7,21 +7,30 @@
         Meri Coleman and T. L. Liau to gauge the understandability
                             of a text.                                */
 
-int num_letters(string);                                    //gets the number of letters in a string
+int count_letters(string);                                    //gets the number of letters in a string
 
 int main(void)
 {
     string text = get_string("Text: ");
-    int n = num_letters(text);
-    printf("%i\n", n);
-    //how many letters are in a sentence
+    int letters = count_letters(text);
+    int words = 0;
+
+    for(int i = 0; i <strlen(text); i++)
+    {
+        if(isspace(text[i]))
+        {
+            words++;
+        }
+    }
+
+    printf("%i\n", words);
 }
 
-int num_letters(string s)
+int count_letters(string s)
 {
      int alpha = 0;
 
-       for(int i =0; i < strlen(s); i++)
+       for(int i =0; i <strlen(s); i++)
        {
            if(isalpha(s[i]))
            {
@@ -30,5 +39,4 @@ int num_letters(string s)
        }
     return alpha;
 }
-
 
