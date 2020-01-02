c943cd2f2a1d71cb22bbda106a80bd870054fb85
Jonathan McKay <jmckay586@gmail.com>
2020-01-02 07:31:35 +0000

    readability.c: working solution for 2019s problemset 2

--- pset2_2019/readability.c
+++ pset2_2019/readability.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
+#include<math.h>
 
 /*      The Coleman–Liau index is a readability test designed by
         Meri Coleman and T. L. Liau to gauge the understandability
@@ -9,22 +10,38 @@
 
 /*                  Sentence boundary detection                        */
 
-
-int count_letters(string);                                              //gets the number of letters in a string
+int count_letters(string);                                          //gets the number of letters in a string
 int count_words(string);                                           //gets the number of words in a string
-int count_sent(string);                                                 //counts number of sentences in a string
+int count_sent(string);                                            //counts number of sentences in a string
 
 
 int main(void)
 {
     string text = get_string("Text: ");
+
     int letters = count_letters(text);
     int words = count_words(text);
-    int punct = count_sent(text);
+    int sentence = count_sent(text);
 
-    printf("%i Letter(s)\n", letters);
-    printf("%i Word(s)\n", words);
-    printf("%i Sentence(s)\n", punct);
+    //applying coleman-liau index
+    float l = (float) letters/words * 100;
+    float s = (float) sentence/words * 100;
+
+    float index = l*0.0588 - s*0.296 - 15.8;
+    int grade = round(index);
+
+    if(grade < 0)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if(grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
 }
 
 int count_letters(string s)
