ed8305d706379a8e9aa5c608b995621d311d464e
Jonathan McKay <jmckay586@gmail.com>
2024-08-06 06:24:37 -0500

    Update readabillity.c

--- pset2_2024/readabillity.c
+++ pset2_2024/readabillity.c
@@ -10,13 +10,15 @@ int count_sentences(string text);
 
 int main(void)
 {
-  string text = get_string("Text: ");
+  string text = get_string("Text: ");           //get users input string
 
-  int letters = count_letters(text);
-  int words = count_words(text);
+  int letters = count_letters(text);            //count the number of letters, store in int letters
+  int words = count_words(text);                //count the number of words, store in int words
+  int sentences = count_sentences(text);        //count the number of sentences, store it in int sentences
 
-  printf("%i\n", letters);
-  printf("%i\n", words);
+  printf("%i\n", letters);                      //print out the number of letters in the users input
+  printf("%i\n", words);                        //print out the number of words in the users input
+  printf("%i\n", sentences);                    //print out the number of sentences in the users input
 }
 
 int count_letters(string text)
@@ -43,4 +45,17 @@ int count_words(string text)
         }
     }
     return words;
+}
+
+int count_sentences(string text)
+{
+    int sentences = 0;
+    for(int i = 0, len = strlen(text); i < len; i++)
+    {
+        if(ispunct(text[i]))
+        {
+            sentences = sentences+1;
+        }
+    }
+    return sentences;
 }
