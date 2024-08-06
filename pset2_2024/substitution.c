6b86d218a80d187d1305b505c0bb634a6fba1395
Jonathan McKay <jmckay586@gmail.com>
2024-08-06 07:43:57 -0500

    Update substitution.c

--- pset2_2024/substitution.c
+++ pset2_2024/substitution.c
@@ -2,8 +2,19 @@
 #include<cs50.h>
 #include<string.h>
 #include<ctype.h>
+#include<stdlib.h>
+
 
 int main(int argc, string argv[])
 {
     string plaintext = get_string("plaintext: ");
+    int key = atoi(argv[1]);
+    printf("%i\n", key);
+
+    for(int i = 0, len = strlen(plaintext); i < len; i++)
+    {
+        plaintext[i] = plaintext[i] + key;
+        printf("%s\n", plaintext);
+    }
+
 }
