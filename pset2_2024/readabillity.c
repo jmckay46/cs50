508190c6fb49278e9c6eac8d80467f43e9107d57
Jonathan McKay <jmckay586@gmail.com>
2024-08-06 06:10:35 -0500

    Update readabillity.c

--- pset2_2024/readabillity.c
+++ pset2_2024/readabillity.c
@@ -1,1 +1,28 @@
+#include<cs50.h>
+#include<ctype.h>
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
 
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+  string text = get_string("Text: ");
+
+  int letters = count_letters(text);
+}
+
+int count_letters(string text)
+{
+   int letters = 0;
+   for(int i = 0, len = strlen(text); i < len; i++)
+{
+    if(isalpha(text))
+   {
+    letters = letters+1;
+   }
+ }
+}
